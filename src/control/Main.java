f218820ba180f4ee8acf2beea07b8fe4d010b9fc
mico77720julio@gmail.com <mico77720julio@gmail.com@1c7d24b8-dc15-165d-a84a-1912db3263d1>
2014-04-28 15:51:53 +0000

    Se agrego la dependencia a jdatepicker, el panel no esta completo.

--- src/control/Main.java
+++ src/control/Main.java
@@ -1,10 +1,12 @@
 package control;
 
+import vista.Ventana;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Ventana ventana = new Ventana();
+		
 	}
 
 }
---
+++ src/vista/PanelConsultaViajes.java
@@ -0,0 +1,18 @@
+package vista;
+
+import javax.swing.JPanel;
+
+import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
+import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
+import net.sourceforge.jdatepicker.impl.UtilDateModel;
+
+public class PanelConsultaViajes extends JPanel {
+	public PanelConsultaViajes() {
+		UtilDateModel model = new UtilDateModel();
+		JDatePanelImpl datePanel = new JDatePanelImpl(model);
+		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel);
+		 
+		this.add(datePicker);
+	}
+
+}

de6676bc38dc085524487192eb18554c2e44da23
leebeth.anacona@gmail.com <leebeth.anacona@gmail.com@1c7d24b8-dc15-165d-a84a-1912db3263d1>
2014-04-28 19:26:20 +0000

    Tab Gerente - Miguel Piza, Julio Campaz, Lorena Anacona

---
+++ trunk/src/vista/DialogEditarCosto.java
@@ -0,0 +1,172 @@
+package Interfaz;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.JRadioButton;
+import javax.swing.JRadioButtonMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class DialogEditarCosto extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	private tabGerente principal;
+	private JTextField txtDescuentos;
+	private JTextField txtOrigen;
+	private JTextField txtDestino;
+	private JTextField textField;
+	private ButtonGroup butGroup;
+	/**
+	 * Create the dialog.
+	 */
+	public DialogEditarCosto(tabGerente ventana) 
+	{
+		setTitle("Editar Costo");
+		principal = ventana;
+
+		setBounds(100, 100, 455, 449);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblDescuentos = new JLabel("Descuentos:");
+			lblDescuentos.setBounds(10, 11, 117, 14);
+			contentPanel.add(lblDescuentos);
+		}
+		{
+			txtDescuentos = new JTextField();
+			txtDescuentos.setBounds(10, 30, 214, 20);
+			contentPanel.add(txtDescuentos);
+			txtDescuentos.setColumns(10);
+		}
+		{
+			JLabel lblCiudades = new JLabel("Ciudades:");
+			lblCiudades.setBounds(10, 61, 117, 14);
+			contentPanel.add(lblCiudades);
+		}
+		{
+			JList ciudadesList = new JList();
+			ciudadesList.setBounds(10, 86, 214, 173);
+			contentPanel.add(ciudadesList);
+		}
+		{
+			JLabel lblOrigen = new JLabel("Origen:");
+			lblOrigen.setBounds(234, 87, 65, 14);
+			contentPanel.add(lblOrigen);
+		}
+		{
+			txtOrigen = new JTextField();
+			txtOrigen.setBounds(234, 108, 157, 20);
+			contentPanel.add(txtOrigen);
+			txtOrigen.setColumns(10);
+		}
+		{
+			ImageIcon image = new ImageIcon("./data/delete.png");
+			JButton butEliminarOrigen = new JButton(image);
+			butEliminarOrigen.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) 
+				{
+					txtOrigen.setText("");
+				}
+			});
+			butEliminarOrigen.setBounds(397, 107, 34, 23);
+			contentPanel.add(butEliminarOrigen);
+		}
+		{
+			JLabel lblDestino = new JLabel("Destino:");
+			lblDestino.setBounds(234, 152, 46, 14);
+			contentPanel.add(lblDestino);
+		}
+		{
+			txtDestino = new JTextField();
+			txtDestino.setBounds(234, 170, 157, 20);
+			contentPanel.add(txtDestino);
+			txtDestino.setColumns(10);
+		}
+		{
+			ImageIcon image = new ImageIcon("./data/delete.png");
+			JButton butEliminarDestino = new JButton(image);
+			butEliminarDestino.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) 
+				{
+					txtDestino.setText("");
+				}
+			});
+			butEliminarDestino.setBounds(397, 167, 34, 23);
+			contentPanel.add(butEliminarDestino);
+		}
+		{
+			JLabel lblTarifa = new JLabel("Tarifa:");
+			lblTarifa.setBounds(10, 270, 100, 14);
+			contentPanel.add(lblTarifa);
+		}
+		{
+			textField = new JTextField();
+			textField.setBounds(10, 289, 214, 20);
+			contentPanel.add(textField);
+			textField.setColumns(10);
+		}
+		{
+			JLabel lblClase = new JLabel("Clase:");
+			lblClase.setBounds(10, 320, 74, 14);
+			contentPanel.add(lblClase);
+		}
+
+		JRadioButton rdbtnAlta = new JRadioButton("Alta", true);
+		rdbtnAlta.setBounds(10, 341, 54, 23);
+		contentPanel.add(rdbtnAlta);
+
+		JRadioButton rdbtnMedia = new JRadioButton("Media");
+		rdbtnMedia.setBounds(88, 341, 65, 23);
+		contentPanel.add(rdbtnMedia);
+
+		JRadioButton rdbtnBaja = new JRadioButton("Baja");
+		rdbtnBaja.setBounds(171, 341, 109, 23);
+		contentPanel.add(rdbtnBaja);
+		
+		butGroup = new ButtonGroup();
+		butGroup.add(rdbtnBaja);
+		butGroup.add(rdbtnMedia);
+		butGroup.add(rdbtnAlta);
+
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) 
+					{
+						dispose();
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
--- trunk/src/vista/Ventana.java
+++ trunk/src/vista/Ventana.java
@@ -1,33 +1,94 @@
 package vista;
 
-import java.awt.GridLayout;
-import java.awt.event.KeyEvent;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
+import java.awt.BorderLayout;
 
 public class Ventana extends JFrame {
-	public Ventana() {
-		//1. Create the frame.
-		super();
-		this.setLayout(new GridLayout(1, 1));
-		//2. Optional: What happens when the frame closes?
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//3. Create components and put them in the frame.
-		//TODO all all panes to
-		JTabbedPane tabbedPane = new JTabbedPane();
-		PanelConsultaViajes consultaViajes = new PanelConsultaViajes();
-        tabbedPane.addTab("Tap 1", consultaViajes);
-        tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
-		
-		
-		//Add the tabbed pane to this panel.
-        add(tabbedPane);
-        //4. Size the frame.
-		this.pack();
-		//5. Show it.
-		this.setVisible(true);
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					tabGerente frame = new tabGerente();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
+	/**
+	 * Create the frame.
+	 */
+	public tabGerente() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 682, 472);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 666, 434);
+		contentPane.add(tabbedPane);
+		
+		Panel panel = new Panel();
+		tabbedPane.addTab("Gerente", null, panel, null);
+		panel.setLayout(null);
+		
+		JLabel lblTiquetes = new JLabel("Tiquetes:");
+		lblTiquetes.setBounds(10, 11, 92, 14);
+		panel.add(lblTiquetes);
+		
+		JList list = new JList();
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setBounds(20, 386, 257, -349);
+		panel.add(list);
+		
+		JLabel lblCosto = new JLabel("Costo:");
+		lblCosto.setBounds(615, 65, 46, 14);
+		panel.add(lblCosto);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setBounds(517, 83, 134, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnEditarCosto = new JButton("Editar Costo");
+		btnEditarCosto.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				DialogEditarCosto dialog = new DialogEditarCosto(Ventana.this);
+				dialog.setVisible(true);
+			}
+		});
+		btnEditarCosto.setBounds(517, 127, 134, 23);
+		panel.add(btnEditarCosto);
+		
+		JButton btnGenerarReporte = new JButton("Generar Reporte");
+		btnGenerarReporte.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				DialogGenerarReporte dialog = new DialogGenerarReporte(Ventana.this);
+				dialog.setVisible(true);
+			}
+		});
+		btnGenerarReporte.setBounds(517, 161, 134, 23);
+		panel.add(btnGenerarReporte);
+		
+		JList list_1 = new JList();
+		list_1.setBounds(10, 36, 483, 359);
+		panel.add(list_1);
+		
+		PanelConsultaViajes panelConsultaViajes = new PanelConsultaViajes();
+		tabbedPane.addTab("Panel Consuta", null, panelConsultaViajes, null);
+	}
 }

aa954f2c5a4d1f2c611a759e5a4ae5b48054e144
leebeth.anacona@gmail.com <leebeth.anacona@gmail.com@1c7d24b8-dc15-165d-a84a-1912db3263d1>
2014-05-13 00:51:48 +0000

    Longitud y latitud ahora son String

--- src/modelo/Destino.java
+++ src/modelo/Destino.java
@@ -3,10 +3,10 @@ package modelo;
 public class Destino
 {
 	private int id;
-	private double latitud;
-	private double longitud;
+	private String latitud;
+	private String longitud;
 	private String descripcion;
-	public Destino(int id, double latitud, double longitud,
+	public Destino(int id, String latitud, String longitud,
 			String descripcion) {
 		super();
 		this.id = id;
@@ -20,16 +20,16 @@ public class Destino
 	public void setId(int id) {
 		this.id = id;
 	}
-	public double getLatitud() {
+	public String getLatitud() {
 		return latitud;
 	}
-	public void setLatitud(double latitud) {
+	public void setLatitud(String latitud) {
 		this.latitud = latitud;
 	}
-	public double getLongitud() {
+	public String getLongitud() {
 		return longitud;
 	}
-	public void setLongitud(double longitud) {
+	public void setLongitud(String longitud) {
 		this.longitud = longitud;
 	}
 	public String getDescripcion() {

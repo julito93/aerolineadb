0e2f3aba022198589f83f211bb9ad8061d4a79e8
csacanam <csacanam@1c7d24b8-dc15-165d-a84a-1912db3263d1>
2014-05-18 19:34:45 +0000

    Se crea la clase Ruta en el modelo

---
+++ src/modelo/Ruta.java
@@ -0,0 +1,72 @@
+package modelo;
+
+import java.util.Date;
+
+public class Ruta
+{
+	
+	private int id;
+	private Date fecha;
+	private String idViaje;
+	private String idTarifa;
+	
+	public Ruta()
+	{
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Ruta(int id, Date fecha, String idViaje, String idTarifa)
+	{
+		this.id = id;
+		this.fecha = fecha;
+		this.idViaje = idViaje;
+		this.idTarifa = idTarifa;
+	}
+
+
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public Date getFecha()
+	{
+		return fecha;
+	}
+
+	public void setFecha(Date fecha)
+	{
+		this.fecha = fecha;
+	}
+
+	public String getIdViaje()
+	{
+		return idViaje;
+	}
+
+	public void setIdViaje(String idViaje)
+	{
+		this.idViaje = idViaje;
+	}
+
+	public String getIdTarifa()
+	{
+		return idTarifa;
+	}
+
+	public void setIdTarifa(String idTarifa)
+	{
+		this.idTarifa = idTarifa;
+	}
+	
+	
+
+}

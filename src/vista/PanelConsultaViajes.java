4cfd669889a9b9b6f45e38234273d2f2fdb82a31
jfdominguezp@gmail.com <jfdominguezp@gmail.com@1c7d24b8-dc15-165d-a84a-1912db3263d1>
2014-05-18 05:28:04 +0000

    Consulta de vuelos terminada

--- src/vista/PanelConsultaViajes.java
+++ src/vista/PanelConsultaViajes.java
@@ -2,25 +2,34 @@ package vista;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.Date;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
 import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
 import net.sourceforge.jdatepicker.impl.UtilDateModel;
+
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.GridLayout;
+
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.factories.FormFactory;
+
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class PanelConsultaViajes extends JPanel implements ActionListener {
 	private JDatePickerImpl inicio, fin;
@@ -62,8 +71,10 @@ public class PanelConsultaViajes extends JPanel implements ActionListener {
 		this.add(fin, "3, 3, fill, fill");
 		this.add(buscar, "2, 4, 2, 1, center, center");
 		
-		table = new JTable();
-		add(table, "2, 5, 2, 1, fill, fill");
+		String[] nombres = {"Vuelo", "Fecha", "Origen", "Destino"};
+		DefaultTableModel model = new DefaultTableModel(nombres, 1);
+		table = new JTable(model);
+		add(new JScrollPane(table), "2, 5, 2, 1, fill, fill");
 		
 		
 	}
@@ -91,6 +102,29 @@ public class PanelConsultaViajes extends JPanel implements ActionListener {
 	public JTable getTable ()
 	{
 		return this.table;
+	}
+	
+	
+	public void actualizarTabla(ResultSet rs){
+	
+		String[] nombres = {"Vuelo", "Fecha", "Origen", "Destino"};
+		DefaultTableModel model = new DefaultTableModel(nombres, 0);
+		try {
+			while(rs.next())
+			{
+				System.out.println("En el while");
+				model.addRow(new Object[]{rs.getString(2), rs.getDate(1).toString(), rs.getString(3), rs.getString(4) });
+				System.out.println("Origen" + rs.getString(3));
+			
+			}
+			this.table.setModel(model);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		this.table.setModel(model);
+		this.table.repaint();
+
 	}
 	
 	@Override

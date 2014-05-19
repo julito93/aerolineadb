56df7636412da84dfc9667efab9a2612ef0cc60f
miguelpiza930@gmail.com <miguelpiza930@gmail.com@1c7d24b8-dc15-165d-a84a-1912db3263d1>
2014-05-19 14:37:55 +0000

    comentarios tablas

---
+++ db/comentarios tablas.sql
@@ -0,0 +1,76 @@
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Pantallas IS 'tabla que contiene la informacion de las pantallas'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Roles IS 'tabla que contiene la informacion de los roles'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Rol_Pantalla IS 'tabla que contiene la informacion de la relacion entre roles y pantallas'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Usuarios IS 'tabla que contiene la informacion de los usuarios'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Ventas IS 'tabla que contiene la informacion de las ventas'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Tiquetes IS 'tabla que contiene la informacion de los tiquetes'
+;
+
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Rutas IS 'tabla que contiene la informacion de las rutas'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Viajes IS 'tabla que contiene la informacion de los viajes'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Vuelos IS 'tabla que contiene la informacion de los vuelos'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Lugares IS 'tabla que contiene la informacion de los lugares'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Descuentos IS 'tabla que contiene la informacion de los descuentos'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Clases IS 'tabla que contiene la informacion de las clases'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Usuario_Rol IS 'tabla que contiene la informacion de la relacion entre usuarios y roles'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Ruta_Vuelo IS 'tabla que contiene la informacion de la relacion entre rutas y vuelos'
+;
+
+-- Table and Columns comments section
+
+COMMENT ON TABLE Tarifas IS 'tabla que contiene la informacion de las tarifas'
+;

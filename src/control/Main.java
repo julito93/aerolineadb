9db8a6690e64da4043b5a10ad7aa7cf8537e59ce
miguelpiza930@gmail.com <miguelpiza930@gmail.com@1c7d24b8-dc15-165d-a84a-1912db3263d1>
2014-05-19 05:02:58 +0000

    adicion caso para triger

--- src/control/Main.java
+++ src/control/Main.java
@@ -607,7 +607,7 @@ public class Main {
 				catch (SQLException e1) 
 				{
 					String[] err = e1.getMessage( ).split( "\n" );
-					if( e1.getErrorCode( ) == 20004 )
+					if( e1.getErrorCode( ) == 20002 || e1.getErrorCode( ) == 20004 )
 						JOptionPane.showMessageDialog( null, err[0], "Error", JOptionPane.ERROR_MESSAGE );
 					else
 						e1.printStackTrace();
